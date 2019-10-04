93f0685856b6e43c4e42fd9de4f6af1f6908bf02
Frans Bouma <frans@sd.nl>
2019-10-04 15:44:10 +0200

    Site updated

--- commitanddeploy.cmd
+++ commitanddeploy.cmd
@@ -1,10 +1,11 @@
+@echo off
 REM This little script assumes the framedsc.github.io is in a folder next to the one this is in (so ..\framedsc.github.io)
 REM It'll generate, copy and commit the changes made in this repo.
-@echo off
 git add -A
 git commit -m "Site updated"
 git push
 docnet -c markdown
+
 del /q ..\framedsc.github.io\*.*
 for /d %i in (..\framedsc.github.io\*.*) do @rmdir /s /q "%i"
 
